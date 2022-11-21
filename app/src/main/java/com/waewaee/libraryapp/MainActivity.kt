d2f7ffd9ebaa8022a77e5106436c1a1ab85de8dc
Wae Waee <waewaee@gmail.com>
2022-11-21 22:26:27 +0630

    initial commit

---
+++ app/src/main/java/com/waewaee/libraryapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waewaee.libraryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
